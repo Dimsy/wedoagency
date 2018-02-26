30bd562965d472e8645ac960cadc6dae954c998a
Oleg Mayurchenko <vfxq@rambler.ru>
2018-02-26 15:59:04 +0300

    Photo rendering done

--- src/components/PortfolioPage/index.js
+++ src/components/PortfolioPage/index.js
@@ -8,6 +8,55 @@ import safeEval from 'safe-eval'
 
 class PortfolioPage extends Component{
 
+
+	a = (photos) => {
+		for (let key in photos){		
+			const photoIndex = key.indexOf('photo')
+
+			const photo = key.substring(photoIndex)
+			photos[photo] = photos[key]
+									 
+		}
+		
+		return (
+			<Row key={photos.photo1+'_'+photos.photo1x2}>
+				<Col md={12}>
+					<img src={`${photos.photo1}`}  srcSet={`${photos.photo1x2} 2x`} alt="Фото проекта"/>
+					<img src={`${photos.photo2}`}  srcSet={`${photos.photo2x2} 2x`} alt="Фото проекта"/>
+					<img src={`${photos.photo3}`}  srcSet={`${photos.photo3x2} 2x`} alt="Фото проекта"/>
+				</Col>
+			</Row>
+		)	
+	}
+
+	b = (photos) => {
+		for (let key in photos){
+			console.log('b', photos[key])
+		
+			const photoIndex = key.indexOf('photo')
+
+			const photo = key.substring(photoIndex)
+			photos[photo] = photos[key]
+									 
+		}
+
+		const styleRight = {
+						display: "inline-block", 
+						float: "right"
+					}
+		
+		return (
+			<Row key={photos.photo1+'_'+photos.photo1x2}>
+				<Col md={12}>
+					<img src={`${photos.photo1}`}  srcSet={`${photos.photo1x2} 2x`} alt="Фото проекта" style={styleRight}/>
+					<img src={`${photos.photo2}`}  srcSet={`${photos.photo2x2} 2x`} alt="Фото проекта" style={styleRight}/>
+					<img src={`${photos.photo3}`}  srcSet={`${photos.photo3x2} 2x`} alt="Фото проекта" style={styleRight}/>
+					<div className="clear" />
+				</Col>
+			</Row>
+		)	
+	}
+
 	c = (photos) => {
 
 		for (let key in photos){
@@ -30,6 +79,8 @@ class PortfolioPage extends Component{
 	}
 
 	d = (photos) => {
+
+		console.log(Object.keys(photos).length)
 			for (let key in photos){
 			const photoIndex = key.indexOf('photo')
 
@@ -38,10 +89,42 @@ class PortfolioPage extends Component{
 									 
 		}
 		
+
 		return (
 			<Row key={photos.photo1+'_'+photos.photo1x2}>
 				<Col md={12}>
 					<img src={`${photos.photo1}`}  srcSet={`${photos.photo1x2} 2x`} alt="Фото проекта"/>
+				</Col>
+			</Row>
+		)	
+	}
+
+	e = (photos) => {
+		for (let key in photos){
+			const photoIndex = key.indexOf('photo')
+			const photo = key.substring(photoIndex)
+
+			photos[photo] = photos[key]						 
+		}
+
+
+		const styleRight = {
+						display: "inline-block", 
+						float: "right"
+					}
+
+		const styleLeft = {
+						display: "inline-block", 
+						float: "left"
+					}				
+		
+		return (
+			<Row key={photos.photo1+'_'+photos.photo1x2}>
+				<Col md={12}>
+					<img src={`${photos.photo1}`}  srcSet={`${photos.photo1x2} 2x`} alt="Фото проекта" style={styleLeft}/>
+					<img src={`${photos.photo2}`}  srcSet={`${photos.photo2x2} 2x`} alt="Фото проекта" style={styleRight}/>
+					<img src={`${photos.photo3}`}  srcSet={`${photos.photo3x2} 2x`} alt="Фото проекта" style={styleRight}/>
+					<img src={`${photos.photo4}`}  srcSet={`${photos.photo4x2} 2x`} alt="Фото проекта" style={styleLeft}/>
 				</Col>
 			</Row>
 		)	
@@ -114,78 +197,6 @@ class PortfolioPage extends Component{
 		 											? <img src={`${project.acf.photoNextText}`}  srcSet={`${project.acf.photoNextTextx2} 2x`} alt="Фото проекта 1"/>
 		 											: null																											 
 
-		// const photo1 = (!!project.acf.photo1) && (project.acf.photo1 != false)
-		// 											? <img src={`${project.acf.photo1}`}  srcSet={`${project.acf.photo1x2} 2x`} alt="Фото проекта 1"/>
-		// 											: null
-																										 
-		// const photo2 = (!!project.acf.photo2) && (project.acf.photo2 != false)  
-		// 											? <img src={`${project.acf.photo2}`}  srcSet={`${project.acf.photo2x2} 2x`} alt="Фото проекта 2"/>
-		// 											: null
-
-		// const photo3 = (!!project.acf.photo3) && (project.acf.photo3 != false) 
-		// 											? <img src={`${project.acf.photo3}`}  srcSet={`${project.acf.photo3x2} 2x`} alt="Фото проекта 3"/>
-		// 		  							  : null			
-
-		// const photo4 = (!!project.acf.photo4) && (project.acf.photo4 != false)
-		// 											? <img src={`${project.acf.photo4}`}  srcSet={`${project.acf.photo4x2} 2x`} alt="Фото проекта 4"/>
-		// 											: null			
-																							 
-		// const photo5 = (!!project.acf.photo5) && (project.acf.photo5 != false) 
-		// 											? <img src={`${project.acf.photo5}`}  srcSet={`${project.acf.photo5x2} 2x`} alt="Фото проекта 5"/>
-		// 											: null			
-		
-		// const photo6 = (!!project.acf.photo6) && (project.acf.photo6 != false) 
-		// 											? <img src={`${project.acf.photo6}`}  srcSet={`${project.acf.photo6x2} 2x`} alt="Фото проекта 6"/>
-		// 											: null			
-	  
-	 //  const photo7 = (!!project.acf.photo7) && (project.acf.photo7 != false)
-	 //  											? <img src={`${project.acf.photo7}`}  srcSet={`${project.acf.photo7x2} 2x`} alt="Фото проекта 7"/>
-		// 											: null			
-	  
-	 //  const photo8 = (!!project.acf.photo8) && (project.acf.photo8 != false)
-	 //  											? <img src={`${project.acf.photo8}`}  srcSet={`${project.acf.photo8x2} 2x`} alt="Фото проекта 8"/>
-		// 											: null			
-	  
-	 //  const photo9 = (!!project.acf.photo9) && (project.acf.photo9 != false)
-	 //  											? <img src={`${project.acf.photo9}`}  srcSet={`${project.acf.photo9x2} 2x`} alt="Фото проекта 9"/>
-		// 											: null			
-	  
-	 //  const photo10 = (!!project.acf.photo10) && (project.acf.photo10 != false)
-	 //  											? <img src={`${project.acf.photo10}`}  srcSet={`${project.acf.photo10x2} 2x`} alt="Фото проекта 10"/>
-		// 											: null			
-	  
-	 //  const photo11 = (!!project.acf.photo11) && (project.acf.photo11 != false)
-	 //  											? <img src={`${project.acf.photo11}`}  srcSet={`${project.acf.photo11x2} 2x`} alt="Фото проекта 11"/>
-		// 											: null			
-	  
-	 //  const photo12 = (!!project.acf.photo12) && (project.acf.photo12 != false)
-	 //  											? <img src={`${project.acf.photo12}`}  srcSet={`${project.acf.photo12x2} 2x`} alt="Фото проекта 12"/>
-		// 											: null		
-
-		// const photo13 = (!!project.acf.photo13) && (project.acf.photo13 != false)
-		// 											? <img src={`${project.acf.photo13}`}  srcSet={`${project.acf.photo13x2} 2x`} alt="Фото проекта 13"/>
-		// 											: null									
-			
-		// const photo14 = (!!project.acf.photo14) && (project.acf.photo14 != false)
-		// 											? <img src={`${project.acf.photo14}`}  srcSet={`${project.acf.photo14x2} 2x`} alt="Фото проекта 14"/>
-		// 											: null	
-																							 
-		// const photo15 = (!!project.acf.photo15) && (project.acf.photo15 != false)
-		// 											? <img src={`${project.acf.photo15}`}  srcSet={`${project.acf.photo15x2} 2x`} alt="Фото проекта 15"/>
-		// 											: null																						 																						 														 
-		
-		// const photo16 = (!!project.acf.photo16) && (project.acf.photo16 != false)
-		// 											? <img src={`${project.acf.photo16}`}  srcSet={`${project.acf.photo16x2} 2x`} alt="Фото проекта 16"/>
-		// 											: null																						 
-
-		// const photo17 = (!!project.acf.photo17) && (project.acf.photo17 != false)
-		// 											? <img src={`${project.acf.photo17}`}  srcSet={`${project.acf.photo17x2} 2x`} alt="Фото проекта 17"/>
-		// 											: null	
-
-		// const photo18 = (!!project.acf.photo18) && (project.acf.photo18 != false)
-		// 											? <img src={`${project.acf.photo18}`}  srcSet={`${project.acf.photo18x2} 2x`} alt="Фото проекта 18"/>
-		// 											: null																						 
-
 		return(
 			<div className="portfolioPage__project">
 				<div className="articleImgNews" style={header} />
@@ -200,23 +211,6 @@ class PortfolioPage extends Component{
 					<Row>
 						<Col md={12} className="photoBlock">
 							{body}
-							{/*photo2}
-							{photo3}
-							{photo4}
-							{photo5}
-							{photo6}
-							{photo7}
-							{photo8}
-							{photo9}
-							{photo10}
-							{photo11}
-							{photo12}
-							{photo13}
-							{photo14}
-							{photo15}
-							{photo16}
-							{photo17}
-							{photo18*/}	
 						</Col>
 					</Row>
 				</Grid>
